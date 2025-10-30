73e7fcca124a10d18a902a07e1febd82ef3365a5
tomcapita80-web <tomcapita80@gmail.com>
2025-10-29 18:33:51 -0600

    Add files via upload

---
+++ project 10.py
@@ -0,0 +1,19 @@
+Name: Wilson
+#Assignment: A01
+#*******************************************************/
+
+
+
+# step1
+print("@..")
+print("(...)")
+print("(<--<")
+print("^^--^^")
+
+
+# step2
+state = input("Us state:")
+capital = input("capital: ")
+print("the capital of " + state + " is   "+ capital)
+
+
